c86920865b03a77492ff78d32ee31c4bdc055227
ha-r6 <ca3994018@gmail.com>
2026-05-05 20:03:02 +0900

    Initial Commit

---
+++ Gacha/GachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachaApp.swift
+//  Gacha
+//
+//  Created by 悠 on 2026/05/05.
+//
+
+import SwiftUI
+
+@main
+struct GachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GachaTests/GachaTests.swift
@@ -0,0 +1,19 @@
+//
+//  GachaTests.swift
+//  GachaTests
+//
+//  Created by 悠 on 2026/05/05.
+//
+
+import Testing
+@testable import Gacha
+
+struct GachaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
